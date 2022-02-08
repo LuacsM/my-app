d9e70dbccf80d58105738d877bf379669d047b58
Lucas Maia <lucas.maia.mn@gmail.com>
2022-02-08 16:36:35 -0400

    tentando verificar token

--- src/auth.js
+++ src/auth.js
@@ -1,26 +1,43 @@
 import axios from "axios";
 
-let status = true
 
-const verificar = () => {
-    const token = localStorage.getItem("token");
-    console.log(token)
-    axios({
+export const isAuthenticate = () => {
+  const token = localStorage.getItem("token");
+  if(token){
+    if (verificar()==="true"){
+      return true;
+    }else return false
+  }else return false
+
+
+  function verificar(){
+
+    try {
+      
+      axios({
         method: 'post',
         url: 'http://localhost:8080/auth/check-token/',
         data: {
-          token: token,
+          "token": token
         }
-      }).then(() =>{
-        status = true;
-
+      }).then(function (response) {
+        //console.log(response);
+        const dados = response.data;
+        if(dados.status){
+          console.log(dados.status)
+          localStorage.setItem('estadoToken', true);
+          
+        }else{
+          localStorage.setItem('estadoToken', false);
+        }
+  
       })
-      .catch(() => {
-        status = false;
-        console.log(status)
-      });
-      
-      return status
-}
+   }
+   finally {
+    console.log(localStorage.getItem('estadoToken'))
+    return localStorage.getItem('estadoToken');
+   }
 
-export const isAuthenticate = () => false;
+    
+  }
+};
